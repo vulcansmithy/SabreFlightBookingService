2f32ca093cb50e44fcf8d235a1098c540ba71308
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2016-01-20 09:17:47 +0800

    Implement a rspec test to confirm the configuration settings are set upon the instatiation of the Session object.

--- app/models/session.rb
+++ app/models/session.rb
@@ -10,5 +10,30 @@ class Session
                 :account_email, 
                 :domain,
                 :ref_message_id
+                
+  def initialize(attributes={})
+    super
+    
+    # retrieve required setting for 'username'
+    raise "Missing required 'username' configuration setting." if ENV["username"].nil?
+    @username = ENV["username"]
+
+    # retrieve required setting for 'password'  
+    raise "Missing required 'password' configuration setting." if ENV["password"].nil?
+    @password = ENV["password"]
+    
+    # retrieve required setting for 'ipcc'  
+    raise "Missing required 'ipcc' configuration setting." if ENV["ipcc"].nil?
+    @ipcc = ENV["ipcc"]
+  
+    # retrieve setting for 'account_email'        
+    raise "Missing 'account_email' configuration setting." if ENV["account_email"].nil?
+    @account_email = ENV["account_email"]
+    
+    # retrieve setting for 'domain'  
+    raise "Missing 'domain' configuration setting." if ENV["domain"].nil?
+    @domain = ENV["domain"]
+
+  end              
 
 end
--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,4 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Session, type: :model do
+  
+  it "should be able to instantiate a new Session and initialize the required configuration settings" do
+    session = Session.new
+
+    expect(session.username     ).to eq ENV["username"     ]
+    expect(session.password     ).to eq ENV["password"     ]
+    expect(session.ipcc         ).to eq ENV["ipcc"         ]
+    expect(session.account_email).to eq ENV["account_email"]
+    expect(session.domain       ).to eq ENV["domain"       ]
+  end
+  
 end
