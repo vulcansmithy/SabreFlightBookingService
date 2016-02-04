8e0bb8d97ed8449218d18353321e7d6170a0986c
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2016-02-04 16:41:51 +0800

    Rebuilding POS section.

--- app/models/bargain_finder_max.rb
+++ app/models/bargain_finder_max.rb
@@ -8,13 +8,51 @@ class BargainFinderMax
   HEADER_ACTION_BARGAIN_FINDER_MAX_RQ = "BargainFinderMaxRQ"
   
   # == Instance methods =======================================================
-  def bfm_one_way(session)
+  def bfm_one_way(session, origin_destination_information)
+    
     namespaces = {
       "xmlns:env" => "http://schemas.xmlsoap.org/soap/envelope/", 
       "xmlns:ns"  => "http://www.opentravel.org/OTA/2002/11",
       "xmlns:mes" => "http://www.ebxml.org/namespaces/messageHeader", 
       "xmlns:sec" => "http://schemas.xmlsoap.org/ws/2002/12/secext"
     }
+    
+    message_body = {
+      "ns:OTA_AirLowFareSearchRQ" => {
+        "ns:POS" => {
+          "ns:Source" => {
+            "ns:RequestorID" => {
+              "ns:CompanyName" => "",
+              :attributes! => {
+                "ns:CompanyName" => {
+                  "Code" => "TN",    
+                },    
+              },
+            },
+            :attributes! => {
+              "ns:RequestorID" => {
+                "Type" => "1",
+                "ID"   => "1",
+              },
+            },
+          },
+          :attributes! => {
+            "ns:Source" => {
+              "PseudoCityCode" => "6A3H",    
+            },    
+          },
+        },
+      },
+      :attributes! => { 
+        "ns:OTA_AirLowFareSearchRQ" => {
+          "Target"          => "Production",
+          "Version"         => "1.9.2",
+          "ResponseType"    => "OTA",
+          "ResponseVersion" => "1.9.2",
+        },
+      },     
+      
+    }  
     
     savon_client = Savon.client(
       wsdl:                    BARGAIN_FINDER_MAX_RQ_WSDL, 
@@ -25,6 +63,8 @@ class BargainFinderMax
       pretty_print_xml:        true,
       convert_request_keys_to: :none
     )
+    
+    response = savon_client.call(:bargain_finder_max_rq, message: message_body)
     
     return savon_client
   end
--- spec/models/bargain_finder_max_spec.rb
+++ spec/models/bargain_finder_max_spec.rb
@@ -6,8 +6,15 @@ RSpec.describe BargainFinderMax, type: :model do
     session = Session.new
     session.create_session_token
     
+    origin_destination_information = {
+      "departure_date_time"  => "",
+      "origin_location"      => "",
+      "destination_location" => "",
+    }
+    
+    
     bfm = BargainFinderMax.new
-    result = bfm.bfm_one_way(session)
+    result = bfm.bfm_one_way(session, origin_destination_information)
   end
 
 end
