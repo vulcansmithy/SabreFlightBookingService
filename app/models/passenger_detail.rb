bc9284015cb730c7e2f8da2f7c70e2566fd2016d
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2016-03-17 00:40:49 +0800

    Fixed the validation error for executing PassengerDetailsRQ.

--- app/models/passenger_detail.rb
+++ app/models/passenger_detail.rb
@@ -17,7 +17,7 @@ class PassengerDetail
   def namespaces
     namespaces = {
       "xmlns:env" => "http://schemas.xmlsoap.org/soap/envelope/", 
-      "xmlns:v"   => "http://services.sabre.com/sp/eab/v3_2", 
+      "xmlns:v"   => "http://services.sabre.com/sp/pd/v3_2", 
       "xmlns:mes" => "http://www.ebxml.org/namespaces/messageHeader", 
       "xmlns:sec" => "http://schemas.xmlsoap.org/ws/2002/12/secext",
     }
@@ -48,6 +48,98 @@ class PassengerDetail
     @savon_client.globals.endpoint(NON_PRODUCTION_ENVIRONMENT_ENDPOINT)  if session.non_production_environment
 
     return @savon_client
+  end
+  
+  def operation_attributes
+    attributes = {
+      "xmlns"         => "http://services.sabre.com/sp/pd/v3_2",
+      "version"       => "3.2.0",
+      "IgnoreOnError" => "false",
+      "HaltOnError"   => "false",
+    }
+    
+    return attributes
+  end
+  
+  def execute_passenger_detail
+
+    message_body = {
+      "v:PostProcessing" => {
+        :@IgnoreAfter          => "false",
+        :@RedisplayReservation => "true",
+        :@UnmaskCreditCard     => "true",
+      },
+  
+      "v:PreProcessing" => {
+        :@IgnoreBefore => "true",
+        "v:UniqueID" => { :@ID => "" },
+      },
+
+      "v:SpecialReqDetails" => {
+        "v:SpecialServiceRQ" => {
+          "v:SpecialServiceInfo" => {
+            "v:AdvancePassenger" => {
+              :@SegmentNumber => "A",
+              "v:Document"    => {
+                :@ExpirationDate => "2018-05-26",
+                :@Number         => "1234567890",
+                :@Type           => "P",
+                
+                "v:IssueCountry"       => "FR",
+                "v:NationalityCountry" => "FR",
+              },
+              "v:PersonName"  => {
+                :@DateOfBirth    => "1980-12-02",
+                :@Gender         => "M",
+                :@NameNumber     => "1.1",
+                :@DocumentHolder => "true",
+                "v:GivenName"  => "JAMES",
+                "v:MiddleName" => "MALCOLM",
+                "v:Surname"    => "GREEN",
+              },
+              "v:VendorPrefs" => {
+                "v:Airline"   => { :@Hosted => "false",  },
+              },
+            },
+          },
+        },
+      },
+      
+      "v:TravelItineraryAddInfoRQ" => {
+        "v:AgencyInfo"   => {
+          "v:Address"  => {
+            "v:AddressLine"     => "SABRE TRAVEL",
+            "v:CityName"        => "SOUTHLAKE",
+            "v:CountryCode"     => "US",
+            "v:PostalCode"      => "76092",     
+            "v:StateCountyProv" => { :@StateCode => "TX", },
+            "v:StreetNmbr"      => "3150 SABRE DRIVE", 
+            "v:VendorPrefs"     => {
+              "v:Airline"       => { :@Hosted => "true", },
+            },
+          },
+        },
+        "v:CustomerInfo" => {
+          "v:ContactNumbers" => {
+            "v:ContactNumber" => {
+              :@NameNumber   => "1.1",
+              :@Phone        => "817-555-1212",
+              :@PhoneUseType => "H", 
+            }, 
+          },
+          "v:PersonName"    =>  {
+            :@NameNumber    => "1.1",   
+            :@NameReference => "ABC123",  
+            :@PassengerType => "ADT", 
+            
+            "v:GivenName"   => "MARCIN",
+            "v:Surname"     => "LACHOWICZ",
+          },
+        },
+      },
+    }
+    
+   response = @savon_client.call(:passenger_details_rq, soap_action: "v:PassengerDetailsRQ", attributes: operation_attributes, message: message_body)
   end
 
 end
--- spec/models/passenger_detail_spec.rb
+++ spec/models/passenger_detail_spec.rb
@@ -13,5 +13,17 @@ RSpec.describe PassengerDetail, type: :model do
     
     expect(passenger_detail.available_operations.first).to eq :passenger_details_rq
   end  
+
+  it "should be able to execute a PassengerDetailRQ" do
+    
+    session = Session.new
+    session.set_to_non_production
+    session.establish_session
+    
+    passenger_detail = PassengerDetail.new
+    passenger_detail.establish_connection(session)
+    
+    passenger_detail.execute_passenger_detail
+  end  
   
 end
