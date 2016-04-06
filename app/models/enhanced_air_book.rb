5e9a47a5afebd8a210408e57fc2cc001f55126e3
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2016-04-06 21:07:59 +0800

    Created a class method that would build a flight_segment_origin_destination_information section.

--- app/models/enhanced_air_book.rb
+++ app/models/enhanced_air_book.rb
@@ -7,6 +7,48 @@ class EnhancedAirBook
   ENHANCED_AIR_BOOK_RQ_WSDL = "http://files.developer.sabre.com/wsdl/sabreXML1.0.00/ServicesPlatform/EnhancedAirBook3.2.0RQ.wsdl"
   ENHANCED_AIR_BOOK_RQ      = "EnhancedAirBookRQ"
   
+  # == Class Methods ==========================================================
+  def self.build_flight_segment_origin_destination_information(args)
+    defaults = {}
+    args.merge!(defaults)
+    
+    flight_segment_section = Hash.new
+    
+    unless args[:@DepartureDateTime].nil?
+      flight_segment_section[:@DepartureDateTime] = args[:@DepartureDateTime]         
+    end 
+    
+    unless args[:@FlightNumber].nil?
+      flight_segment_section[:@FlightNumber] = args[:@FlightNumber]         
+    end     
+    
+    unless args[:@NumberInParty].nil?
+      flight_segment_section[:@NumberInParty] = args[:@NumberInParty]         
+    end   
+    
+    unless args[:@ResBookDesigCode].nil?
+      flight_segment_section[:@ResBookDesigCode] = args[:@ResBookDesigCode]         
+    end  
+    
+    unless args[:@Status].nil?
+      flight_segment_section[:@Status] = args[:@Status]         
+    end  
+
+    unless args[:@LocationCodeDestinationLocation].nil?
+      flight_segment_section[:DestinationLocation] = { :@LocationCode => args[:@LocationCodeDestinationLocation] }
+    end  
+
+    if args[:@CodeMarketingAirline].nil? == false || args[:@FlightNumber].nil? == false
+      flight_segment_section[:MarketingAirline] = { :@Code => args[:@CodeMarketingAirline], :@FlightNumber => args[:@FlightNumber] }
+    end  
+    
+    unless args[:@LocationCodeOriginLocation].nil?
+      flight_segment_section[:OriginLocation] = { :@LocationCode => args[:@LocationCodeOriginLocation] }
+    end  
+    
+    return flight_segment_section
+  end
+  
   # == Instance methods =======================================================
   def initialize
     @savon_client = nil
@@ -62,7 +104,7 @@ class EnhancedAirBook
   
   def execute_enhanced_air_book 
     
-    flight_segment_origin_destination_information = {
+    flight_segment_origin_destination_information = EnhancedAirBook.build_flight_segment_origin_destination_information(
       :@DepartureDateTime               => "2016-06-05T17:05:00",
       :@FlightNumber                    => "764",
       :@NumberInParty                   => "1",
@@ -71,21 +113,21 @@ class EnhancedAirBook
       :@LocationCodeDestinationLocation => "SIN",
       :@CodeMarketingAirline            => "3K",
       :@LocationCodeOriginLocation      => "MNL",
-    }
-    
+    )
+
     begin
       @message_body = {
         "v:OTA_AirBookRQ" => {
           "v:OriginDestinationInformation" => {
             "v:FlightSegment" => {
-              :@DepartureDateTime     => flight_segment_origin_destination_information[:@DepartureDateTime],
-              :@FlightNumber          => flight_segment_origin_destination_information[:@FlightNumber     ],
-              :@NumberInParty         => flight_segment_origin_destination_information[:@NumberInParty    ],
-              :@ResBookDesigCode      => flight_segment_origin_destination_information[:@ResBookDesigCode ],
-              :@Status                => flight_segment_origin_destination_information[:@Status           ],
-              "v:DestinationLocation" => { :@LocationCode => flight_segment_origin_destination_information[:@LocationCodeDestinationLocation] },
-              "v:MarketingAirline"    => { :@Code         => flight_segment_origin_destination_information[:@CodeMarketingAirline           ], :@FlightNumber => flight_segment_origin_destination_information[:@FlightNumber] },
-              "v:OriginLocation"      => { :@LocationCode => flight_segment_origin_destination_information[:@LocationCodeOriginLocation     ] },
+              :@DepartureDateTime     => flight_segment_origin_destination_information[:@DepartureDateTime ],
+              :@FlightNumber          => flight_segment_origin_destination_information[:@FlightNumber      ],
+              :@NumberInParty         => flight_segment_origin_destination_information[:@NumberInParty     ],
+              :@ResBookDesigCode      => flight_segment_origin_destination_information[:@ResBookDesigCode  ],
+              :@Status                => flight_segment_origin_destination_information[:@Status            ],
+              "v:DestinationLocation" => flight_segment_origin_destination_information[:DestinationLocation],
+              "v:MarketingAirline"    => flight_segment_origin_destination_information[:MarketingAirline   ], 
+              "v:OriginLocation"      => flight_segment_origin_destination_information[:OriginLocation     ],
             },
           },
         },
@@ -98,6 +140,8 @@ class EnhancedAirBook
           "v:UniqueID" => { :@ID => "" },
         },
       }
+      
+      puts "@DEBUG #{__LINE__}    @message_body=#{ap @message_body}"
     
       call_response = @savon_client.call(
         :enhanced_air_book_rq, 
