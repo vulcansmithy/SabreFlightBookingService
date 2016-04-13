03d9161fcc543fad2e712e8c570e5553f39dee27
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2016-04-14 06:45:49 +0800

    Cleanup the routes.

--- spec/requests/api/v1/passenger_detail_controller_spec.rb
+++ spec/requests/api/v1/passenger_detail_controller_spec.rb
@@ -1,5 +1,13 @@
 require "rails_helper"
 
 describe Api::V1::PassengerDetailController do
+  
+  it "should be able to successfully call the PassengerDetail API endpoint" do
+    
+    # establish a new Sabre session
+    post "/api/sabre_session/create_session"
+    expect(response.code).to eq "201"
+    
+  end
 
 end
