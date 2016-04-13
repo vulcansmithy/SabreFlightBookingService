904534583ad72b61568c08803b524bcd2ad43309
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2016-04-13 20:31:34 +0800

    Setup API base controller.

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::BaseController < ApplicationController
+  
+  # == Instance methods =======================================================
+  def success_response(data, status_code=:ok)
+    render json: data, status: status_code
+  end
+  
+  def error_response(message, status_code)    
+    render json: { :message => message }, :status => status_code
+  end
+  
 end
