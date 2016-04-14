31d787f703f36984e1ab6662e78b2ce3c3da511e
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2016-04-15 06:03:39 +0800

    Flesh out the details for the EnhancedAirBook API endpoint.

--- app/controllers/api/v1/enhanced_air_book_controller.rb
+++ app/controllers/api/v1/enhanced_air_book_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::EnhancedAirBookController < Api::V1::BaseController
+  
+  # == Constants ==============================================================
+  MISSING_REQUIRED_FLIGHT_SEGMENT_INFO      = "Missing required flight segment(s) information."
+  UNABLE_TO_DO_AN_ENHANCED_AIR_BOOK_REQUEST = "Request failed. Was not able to execute an Enhanced Air Book request."
+  
+  # == API Endpoints ==========================================================
+  # POST /api/enhanced_air_book/execute_enhanced_air_book
+  # POST /api/enhanced_air_book/execute_enhanced_air_book, {}, { "Accept" => "application/vnd.deferointernational.com; version=1" }
+  # POST /api/enhanced_air_book/execute_enhanced_air_book?version=1
+  # POST /api/v1/enhanced_air_book/execute_enhanced_air_book
+  def execute_enhanced_air_book
+    
+    raise MISSING_REQUIRED_SABRE_SESSION_TOKEN if params[:sabre_session_token].nil?
+    sabre_session_token = params[:sabre_session_token]
+    
+    raise MISSING_REQUIRED_DOCUMENT_ADVANCE_PASSENGER_INFO if params[:flight_segments].nil?
+    flight_segments = params[:flight_segments]
+
+    session = SabreSession.new 
+    session.set_to_non_production
+    
+    call_result = session.establish_session(sabre_session_token)
+    raise UNABLE_TO_ESTABLISH_A_SABRE_SESSION if call_result[:status] == :failed
+    
+    enhanced_air_book = EnhancedAirBook.new
+    enhanced_air_book.establish_connection(session)
+    
+    call_result = enhanced_air_book.execute_enhanced_air_book(flight_segments: flight_segments)
+    
+    if call_result[:status] == :success
+      success_response(call_result[:data], :created)  
+    else
+      error_response(UNABLE_TO_DO_AN_ENHANCED_AIR_BOOK_REQUEST, :bad_request)  
+    end 
+  end  
+  
 end
--- spec/requests/api/v1/enhanced_air_book_controller_spec.rb
+++ spec/requests/api/v1/enhanced_air_book_controller_spec.rb
@@ -1,5 +1,38 @@
 require "rails_helper"
 
 describe Api::V1::EnhancedAirBookController do
+  
+  it "should be able to successfully call the EnhancedAirBook API endpoint" do
+    
+    # establish a new Sabre session
+    post "/api/sabre_session/create_session"
+    expect(response.code).to eq "201"
+
+    flight_segments = []
+    flight_segments << EnhancedAirBook.build_flight_segment_origin_destination_information(
+      :@DepartureDateTime               => "2016-06-05T17:05:00",
+      :@FlightNumber                    => "764",
+      :@NumberInParty                   => "1",
+      :@ResBookDesigCode                => "H",
+      :@Status                          => "NN",
+      :@LocationCodeDestinationLocation => "SIN",
+      :@CodeMarketingAirline            => "3K",
+      :@LocationCodeOriginLocation      => "MNL",
+    )
+    
+    payload = {
+      :sabre_session_token => JSON.parse(response.body)["binary_security_token"],
+      :flight_segments     => flight_segments
+    }
+    
+    # call the Passenger Detail API endpoint
+    post "/api/enhanced_air_book/execute_enhanced_air_book", payload
+    expect(response.code).to eq "201"
+    
+    enhanced_air_book = JSON.parse(response.body)["enhanced_air_book"]
+    expect(enhanced_air_book.nil?).to eq false
+    
+    puts "@DEBUG #{__LINE__}    #{ap enhanced_air_book}"
+  end
 
 end
