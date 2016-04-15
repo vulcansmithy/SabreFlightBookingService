266d3ca7840b2b7446ab0aa088ea48da8574e5c9
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2016-04-15 08:15:39 +0800

    Further improvement on the rspec test.

--- spec/requests/api/v1/sabre_session_controller_spec.rb
+++ spec/requests/api/v1/sabre_session_controller_spec.rb
@@ -7,12 +7,17 @@ describe Api::V1::SabreSessionController do
     # call the API endpoint
     post "/api/sabre_session/create_session"
     
+    # expect a return code of successfully created or a response code equavalent to '201'
     expect(response.code).to eq "201"
 
+    # retrieve the returned binary_security_token
     binary_security_token = JSON.parse(response.body)["binary_security_token"]
-    expect(binary_security_token.nil?).to eq false
     
-    puts "@DEBUG #{__LINE__}    binary_security_token='#{binary_security_token}'"
+    # make sure binary_security_token is present and not have nil value
+    expect(binary_security_token.nil?).to eq false
+
+    # make sure the binary_security_token has the sub string value 'Shared/IDL:IceSess'  
+    expect(binary_security_token.scan(/\AShared\/IDL:IceSess/).empty?).to eq false
   end
 
 end
