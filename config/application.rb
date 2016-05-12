def23ae79451ff4830fb49a85a3f27f4ef304ca4
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2016-05-12 23:48:42 +0800

    Removing config.web_console.development_only.

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,5 @@ module ConSolApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
-    # config.web_console.development_only = true
   end
 end
